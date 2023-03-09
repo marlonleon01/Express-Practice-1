82140614472d25115637cdc47070820214866828
Marlon Leon <ghostveene@gmail.com>
2023-03-09 09:56:57 -0500

    changed to what I wrote since it differs

--- final versions/04-express-app.js
+++ final versions/04-express-app.js
@@ -1,19 +1,13 @@
-const express = require('express')
-const path = require('path')
-
+const express = require("express")
+const path = require("path")
 const app = express()
 
-// setup static and middleware
-app.use(express.static('./public'))
+app.use(express.static("./navbar-app"))
 
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"))
 })
 
-app.all('*', (req, res) => {
-  res.status(404).send('resource not found')
-})
+app.all("*", () => res.status(404).send("resournce not found"))
 
-app.listen(5000, () => {
-  console.log('server is listening on port 5000....')
-})
+app.listen(5000, () => console.log("Server is listening on port 5000"))
